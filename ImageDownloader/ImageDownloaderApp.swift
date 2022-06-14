b7baedd0d619302c76974ab29d816e16577a9a7e
Tak Liu <tliu@connected.io>
2022-06-14 16:42:37 -0400

    initial commit

---
+++ ImageDownloader/ImageDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloaderApp.swift
+//  ImageDownloader
+//
+//  Created by Tak Liu on 2022-06-14.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
